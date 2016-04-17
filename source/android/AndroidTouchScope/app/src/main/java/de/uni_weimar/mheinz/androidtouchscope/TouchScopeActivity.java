538cb10e0238c9ffdd5b6c072a6720f87de6d5b3
mheinz54 <mheinz54@gmail.com>
2016-04-17 22:56:35 +0200

    DrawerLayout start, want to add scope controls here

--- source/android/AndroidTouchScope/app/src/main/java/de/uni_weimar/mheinz/androidtouchscope/TouchScopeActivity.java
+++ source/android/AndroidTouchScope/app/src/main/java/de/uni_weimar/mheinz/androidtouchscope/TouchScopeActivity.java
@@ -1,5 +1,9 @@
 package de.uni_weimar.mheinz.androidtouchscope;
 
+import android.graphics.Color;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,6 +13,8 @@ import android.view.MenuItem;
 import android.view.View;
 
 import android.os.Handler;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.support.v7.widget.Toolbar;
@@ -25,6 +31,9 @@ public class TouchScopeActivity extends AppCompatActivity
     private BaseScope mActiveScope = null;
     private ScopeView mScopeView = null;
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList; // temp till done
+
     private Handler mRefreshHandler = new Handler();
 
     @Override
@@ -35,7 +44,22 @@ public class TouchScopeActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.scope_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout.setScrimColor(Color.TRANSPARENT);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayShowTitleEnabled(false);
+          //  actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        mDrawerList = (ListView)findViewById(R.id.right_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                new String[]{"one", "two", "three"}));
 
         mScopeView = (ScopeView) findViewById(R.id.scopeView);
         ToggleButton readButton = (ToggleButton)findViewById(R.id.testRead);
@@ -128,23 +152,20 @@ public class TouchScopeActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_touch_scope, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
         int id = item.getItemId();
-
-        if (id == R.id.action_scope)
+        if(id == R.id.action_rightDrawer)
         {
-            initScope(true);
-            return true;
+            mDrawerLayout.openDrawer(GravityCompat.END);
         }
-        else if(id == R.id.action_test)
+        else
         {
-            initScope(false);
-            return true;
+            mDrawerLayout.closeDrawer(GravityCompat.END);
         }
 
         return super.onOptionsItemSelected(item);
